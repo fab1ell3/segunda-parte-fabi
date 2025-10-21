623110f1074da7d0d479aaa2e113eb1396cb12cf
fab1ell3 <fabielle.santos.silva@escola.pr.gov.br>
2025-10-21 12:17:19 -0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form>
+<h3> cadastro </h3>
+<input type="text" placeholder="login" id="login">
+<input type="password" placeholder="senha" id="senha">
+<input type="email" placeholder="email" >
+
+    
+</h3>
+
+</form>
+</body>
+</html>
